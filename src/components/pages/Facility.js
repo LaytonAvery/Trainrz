9b743d8bc06107558340af15e1430f4842d5b939
LaytonAvery <laytonavery5@gmail.com>
2021-08-08 01:37:53 -0500

    added amenities

--- src/components/pages/Facility.js
+++ src/components/pages/Facility.js
@@ -2,5 +2,19 @@ import React from "react";
 import "../../App.css";
 
 export default function Facility() {
-	return <h1 className='facility'>Facility</h1>;
+	return <>
+	<h1 className='facility'>Facility</h1>
+	<p className="facility-para">Our facility boasts many features. Not only does Trainrz have group fitness classes for yoga enthusiasts, we also have a dedicated boxing area for those who want to spar. Also, Trainrz features cardio equipment, cable machines, and free weights as well as a powerlifting area! Please call 877-999-3751 to schedule your tour today. Complimentary training session included.</p>
+	<ul>
+		<li className="amenities">AMENITIES:</li>
+		<li className="amenities">Sauna</li>
+		<li className="amenities">Showers</li>
+		<li className="amenities">Pool</li>
+		<li className="amenities">Hydromassage</li>
+		<li className="amenities">Protein Shakes and Bars</li>
+		<li className="amenities">Juice Bar</li>
+		<li className="amenities">Cardio Cinema</li>
+		<li className="amenities">Childcare Area</li>
+	</ul>
+	</>
 }
--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -2,5 +2,8 @@ import React from "react";
 import "../../App.css";
 
 export default function SignUp() {
-	return <h1 className='sign-up'>Sign Up</h1>;
+	return <>
+	<h1 className='sign-up'>Sign Up</h1>
+	<p className="sign-up-para">Please see one of our Trainrz to sign up today!</p>
+	</>
 }
