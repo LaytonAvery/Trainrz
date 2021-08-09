597e6efd24a0fefd7d4450f2f84e5fccb8f8c65c
LaytonAvery <laytonavery5@gmail.com>
2021-08-08 20:55:54 -0500

    adding services page

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -2,5 +2,8 @@ import React from "react";
 import "../../App.css";
 
 export default function Services() {
-	return <h1 className='services'>SERVICES</h1>;
+	return <>
+	<h1 className='services'>SERVICES</h1>
+	<p className="services-para">The Trainrz facility has many services to fit all your fitness needs. From one-on-one personal training to nutrition counseling, we aim to aid you in every step along your fitness journey. We start all of our members off with a free one-on-one personal training consultation. This includes a one hour workout session with one of our Trainrz as well as BMI calculation and a personalized nutrition plan. By taking into account your lifestyle outside of the gym, we are able to formulate a plan to help you meet your fitness goals. </p>
+	</>
 }
